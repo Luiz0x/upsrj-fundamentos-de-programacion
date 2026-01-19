6e8b5d30cae46b6525d690e1fa8dadeef2ebeeae
Luiz0x <luisrz2077@gmail.com>
2026-01-19 11:47:11 -0600

    Entrega area y volumen

--- src/main.c
+++ src/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
-int main(int argc, char *argv)
-{
-    printf("Hello world!\n");
+int main() {
+    float side, area, volume;
+
+    
+    printf("Enter the side length: ");
+    
+    // Epic Insane scan
+    if (scanf("%f", &side) != 1) {
+        
+        return 1;
+    }
+
+    // Insane Calculations
+    area = side * side;
+    volume = side * side * side;
+
+    printf("\nResults:\n");
+    printf("- Square area is: %.2f\n", area);
+    printf("- Volume cube is: %.2f\n", volume);
+
     return 0;
 }
